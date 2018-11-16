d255a10af37e4680b8a603db6871ba34f8ffb0bd
artem <temadivnich@gmail.com>
2018-11-16 11:44:53 +0200

    first test

---
+++ src/main/java/org/snooker/api/SnookerApiService.java
@@ -0,0 +1,10 @@
+package org.snooker.api;
+
+public class SnookerApiService {
+
+    private static final String ENDPOINT = "http://api.snooker.org/?e=397&r=1&n=5";
+
+    public String getEndpoint() {
+        return ENDPOINT;
+    }
+}
